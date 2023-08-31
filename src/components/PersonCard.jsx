30b25158d8d997d8a544c64f7fbd09a6b65315fd
Navneet Kaur <n.kaur.sjsu@gmail.com>
2023-08-31 15:04:20 -0400

    added birthday button

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 const PersonCard = (props) => {
+
     const { fullName, age, hairColor } =props;
+
+    const [birthday, setBirthday ] = useState({
+        birthdayCount: age
+    })
+
+    const birthdayHandler = () => {
+        setBirthday({
+            birthdayCount : birthday.birthdayCount + 1 
+        })
+    }
+
     return(
         <div>
             <h1 style={{color:"black"}}> {fullName} </h1>
-            <p>Age: {age}</p>
+            <p>Age: {birthday.birthdayCount}</p>
             <p>Hair Color: {hairColor}</p>
+            
+            <button onClick={ birthdayHandler }  >✨🎂✨Birthday Button for 🥳 {fullName} </button>
         </div>
     );
 }
